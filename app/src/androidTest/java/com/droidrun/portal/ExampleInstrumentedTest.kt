9b192ba7e90b6d6d981dd46086feb58398a47535
exfeipingli20231012 <147703593+exfeipingli20231012@users.noreply.github.com>
2025-05-22 10:26:35 +0800

    Update ExampleInstrumentedTest.kt change droidrun to droidagent

--- app/src/androidTest/java/com/droidrun/portal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/droidrun/portal/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.droidrun.portal
+package com.droidagent.portal
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.droidrun.portal", appContext.packageName)
+        assertEquals("com.droidagent.portal", appContext.packageName)
     }
 }
